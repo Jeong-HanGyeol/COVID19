1f34561d8764a32589e9f0596203e300428da407
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-09 15:18:34 +0900

    차트 디테일 뷰 연결 완료 및 로딩중 구현

--- COVID19/CovidDetailViewController.swift
+++ COVID19/CovidDetailViewController.swift
@@ -17,9 +17,23 @@ class CovidDetailViewController: UITableViewController {
     @IBOutlet weak var totalCaseCell: UITableViewCell!
     @IBOutlet weak var newCaseCell: UITableViewCell!
     
+    var covidOverview: CovidOverview?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureView()
+    }
+    
+    func configureView() {
+        guard let covidOverview = self.covidOverview else { return }
+        self.title = covidOverview.countryName
+        self.newCaseCell.detailTextLabel?.text = "\(covidOverview.newCase)명"
+        self.totalCaseCell.detailTextLabel?.text = "\(covidOverview.totalCase)명"
+        self.recoveredCell.detailTextLabel?.text = "\(covidOverview.recovered)명"
+        self.deathCell.detailTextLabel?.text = "\(covidOverview.death)명"
+        self.percentageCell.detailTextLabel?.text = "\(covidOverview.percentage)%"
+        self.overseassInFlowCell.detailTextLabel?.text = "\(covidOverview.newFcase)명"
+        self.regionalOutbreakCell.detailTextLabel?.text = "\(covidOverview.newCcase)명"
     }
 
 }
